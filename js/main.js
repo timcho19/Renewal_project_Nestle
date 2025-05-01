03da9aa4a93bd4dc4e0163d359c955344e462a8d
Ha-im <gungsun1@naver.com>
2025-05-01 21:34:11 +0900

    scrollTo 추가

--- js/main.js
+++ js/main.js
@@ -2,12 +2,18 @@
 const topBnt = document.querySelector('.top_button');
 const topBntoffset = topBnt.offsetTop;
 
-window.addEventListener('scroll',function(){
+window.addEventListener('scroll',()=>{
     let scrollAmt = window.scrollY;
     if(scrollAmt > topBntoffset){
         topBnt.classList.add('btn');
     }else{
         topBnt.classList.remove('btn');
     }
+})
+topBnt.addEventListener('click',(e)=>{
+    e.preventDefault();
+    window.scrollTo({
+        left:0,top:0,behavior:"smooth"
+    })
 })
 // /top button
