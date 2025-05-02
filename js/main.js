db627a1f2f6e4c602efb7e023fe1fa064eaf5f3e
Ha-im <gungsun1@naver.com>
2025-05-02 16:51:44 +0900

    슬라이드 css js 추가

--- js/main.js
+++ js/main.js
@@ -1,10 +1,46 @@
-// top_button
+const brandTotalWidth = document.body.offsetWidth;
+const brands = document.querySelector('.brands');
+const slid = brands.querySelector('ul');
+const brandSlids = slid.querySelectorAll('li');
 const topBnt = document.querySelector('.top_button');
-const topBntoffset = topBnt.offsetTop;
+const brandsOffsetTop = brands.offsetTop;
+const slidewidth = 292;
+// brands 무한슬라이드
+/*
+for(let slide of brandSlids){
+    slide.style.width = brandTotalWidth + 'px';
+}
+    */
+let slideHTML = slid.innerHTML;
+console.log(slideHTML);
+let clonedSlide = slideHTML.replace(/li/g,'li class="clon"');
+console.log(clonedSlide);
+
+slid.innerHTML = clonedSlide + slideHTML;
+
+let newbrandList = slid.querySelectorAll('li');
+slid.style.width =  slidewidth * newbrandList.length + 'px';
+
+let brandLeft = 0;
+
+function movebrand(){
+    brandLeft -= 2
+    if(brandLeft < -slidewidth * newbrandList.length/2){
+        brandLeft = 0;
+    }
+    slid.style.left = brandLeft + 'px';
+    requestAnimationFrame(movebrand);
+}
+requestAnimationFrame(movebrand);
+
+
+// /brands 무한슬라이드
+
+// top_button
 
 window.addEventListener('scroll',()=>{
     let scrollAmt = window.scrollY;
-    if(scrollAmt > topBntoffset){
+    if(scrollAmt > brandsOffsetTop){
         topBnt.classList.add('btn');
     }else{
         topBnt.classList.remove('btn');
@@ -16,4 +52,5 @@ topBnt.addEventListener('click',(e)=>{
         left:0,top:0,behavior:"smooth"
     })
 })
+
 // /top button
